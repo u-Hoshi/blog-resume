16b9dae81e39fb31daf47c793dd46031a8de8d4f
u-Hoshi <yuta000707@yahoo.co.jp>
2023-11-26 14:49:51 +0900

    lang属性を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="ja">
       <body>
         <KumaRegistry>{children}</KumaRegistry>
       </body>
