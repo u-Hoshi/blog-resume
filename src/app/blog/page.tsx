d3d31a6b3ef6b6699f0046e7f1245bd92e474104
u-Hoshi <yuta000707@yahoo.co.jp>
2023-12-02 13:53:54 +0900

    microcmsの導入

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <>blog</>;
+import { Link } from "@kuma-ui/core";
+import { getList } from "../../../libs/microcms";
+
+export default async function StaticPage() {
+  const { contents } = await getList();
+  const time = new Date().toLocaleString();
+
+  if (!contents || contents.length === 0) {
+    return <h1>No contents</h1>;
+  }
+
+  return (
+    <div>
+      <h1>{time}</h1>
+      <ul>
+        {contents.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/blog/${post.id}`}>{post.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
