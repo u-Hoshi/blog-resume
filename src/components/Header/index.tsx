e3f9f140d295d34b0b3e9ae35052c29db4d89d8a
u-Hoshi <yuta000707@yahoo.co.jp>
2023-12-06 21:07:43 +0900

    ヘッダーの各項目からアイコンを削除

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,10 @@
 import { Flex, Link, styled } from "@kuma-ui/core";
-import { FunctionComponent } from "react";
-import { FaBook, FaHome } from "react-icons/fa";
-import { ImAddressBook } from "react-icons/im";
 
 export const runtime = "edge";
 
 type LinkItem = {
   href: string;
   text: string;
-  Icon: FunctionComponent;
 };
 
 const HeaderList = styled.ul`
@@ -35,9 +31,9 @@ const linkStyles = {
 
 // TODO 除外ルールを外す
 const links: LinkItem[] = [
-  { href: "/", text: "home", Icon: FaHome }, // eslint-disable-line
-  { href: "/resume", text: "resume", Icon: ImAddressBook }, // eslint-disable-line
-  { href: "/blog", text: "blog", Icon: FaBook }, // eslint-disable-line
+  { href: "/", text: "home" },
+  // { href: "/resume", text: "resume", },
+  { href: "/blog", text: "blog" },
 ];
 
 const Header = () => (
@@ -46,7 +42,6 @@ const Header = () => (
       {links.map(({ href, text }) => (
         <ListItem key={href}>
           <Link style={linkStyles} href={href}>
-            {/* <Icon style={{ padding: "2px" }} /> */}
             {text}
           </Link>
         </ListItem>
